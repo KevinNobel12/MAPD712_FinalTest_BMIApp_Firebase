47227107638af0aaad3f026a284f1b21268c5e7e
Kevin Nobel <kev17nobel@gmail.com>
2019-12-11 18:56:47 -0500

    Initial Commit

---
+++ MAPD714_FinalTest_BMICalculatorApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MAPD714_FinalTest_BMICalculatorApp
+//
+//  Created by Kevin Nobel on 2019-12-11.
+//  Copyright © 2019 Kevin Nobel. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MAPD714_FinalTest_BMICalculatorApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MAPD714_FinalTest_BMICalculatorApp
+//
+//  Created by Kevin Nobel on 2019-12-11.
+//  Copyright © 2019 Kevin Nobel. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
